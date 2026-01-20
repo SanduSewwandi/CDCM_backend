32689ac2f54aaa5bf6b89ce8276f7b26a7fab148
SanduSewwandi <sandunis17sewwandi@gmail.com>
2026-01-20 23:31:29 +0530

    new2

--- demo/src/main/java/com/example/demo/dto/LoginResponse.java
+++ demo/src/main/java/com/example/demo/dto/LoginResponse.java
@@ -1,4 +1,17 @@
 package com.example.demo.dto;
 
 public class LoginResponse {
+    private String message;
+    private String doctorId;
+
+    public LoginResponse(String message, String doctorId) {
+        this.message = message;
+        this.doctorId = doctorId;
+    }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public String getDoctorId() { return doctorId; }
+    public void setDoctorId(String doctorId) { this.doctorId = doctorId; }
 }
