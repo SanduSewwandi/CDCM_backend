13efe3479d193c694349bab6be047be496afbe82
Hiru_Iresha <hiruireshasewwandi@gmail.com>
2026-01-21 03:18:43 +0530

    Add admin and hospital login controllers, DTOs, services

--- demo/src/main/java/com/example/demo/dto/LoginRequest.java
+++ demo/src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,8 +1,7 @@
 package com.example.demo.dto;
 
 public class LoginRequest {
-<<<<<<< HEAD
-=======
+
     private String email;
     private String password;
 
@@ -11,5 +10,5 @@ public class LoginRequest {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
->>>>>>> 89e3041278f620dff59a1d43465db79ea34a6c27
+
 }
