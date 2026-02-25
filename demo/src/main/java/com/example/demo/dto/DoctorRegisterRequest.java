dfedccb6c37ebbe68f5ad754232734ff927bce98
SanduSewwandi <sandunis17sewwandi@gmail.com>
2026-02-26 00:24:50 +0530

    new

--- demo/src/main/java/com/example/demo/dto/DoctorRegisterRequest.java
+++ demo/src/main/java/com/example/demo/dto/DoctorRegisterRequest.java
@@ -1,81 +1,64 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.*;
+
 public class DoctorRegisterRequest {
 
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "07\\d{8}", message = "Phone number must start with 07 and be 10 digits")
     private String phone;
+
+    @NotBlank(message = "Specialization is required")
     private String specialization;
+
+    @NotBlank(message = "Medical license number is required")
     private String medicalLicenseNumber;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+            message = "Password must contain uppercase, lowercase, number, and special character"
+    )
     private String password;
 
     public DoctorRegisterRequest() {}
 
     // Getters and Setters
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public String getMedicalLicenseNumber() { return medicalLicenseNumber; }
+    public void setMedicalLicenseNumber(String medicalLicenseNumber) { this.medicalLicenseNumber = medicalLicenseNumber; }
 
-    public String getPhone() {
-        return phone;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public String getMedicalLicenseNumber() {
-        return medicalLicenseNumber;
-    }
-
-    public void setMedicalLicenseNumber(String medicalLicenseNumber) {
-        this.medicalLicenseNumber = medicalLicenseNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
